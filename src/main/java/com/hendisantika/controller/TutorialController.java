eee2557595c6cc261e1bbf5a01ac87a00e8bdec6
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-03 11:25:48 +0700

    #17 - Add updateTutorial API in TutorialController :muscle:

--- src/main/java/com/hendisantika/controller/TutorialController.java
+++ src/main/java/com/hendisantika/controller/TutorialController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -64,5 +65,17 @@ public class TutorialController {
     public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial) {
         Tutorial _tutorial = tutorialRepository.save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), true));
         return new ResponseEntity<>(_tutorial, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/tutorials/{id}")
+    public ResponseEntity<Tutorial> updateTutorial(@PathVariable("id") long id, @RequestBody Tutorial tutorial) {
+        Tutorial _tutorial = tutorialRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found Tutorial with id = " + id));
+
+        _tutorial.setTitle(tutorial.getTitle());
+        _tutorial.setDescription(tutorial.getDescription());
+        _tutorial.setPublished(tutorial.isPublished());
+
+        return new ResponseEntity<>(tutorialRepository.save(_tutorial), HttpStatus.OK);
     }
 }
