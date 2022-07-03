bd5a36e10f34eccdc2c1223c803a41cd5060e114
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-03 11:19:17 +0700

    #6 - Add Tag Entity

---
+++ src/main/java/com/hendisantika/entity/Tag.java
@@ -0,0 +1,53 @@
+package com.hendisantika.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-many-to-many-sample1
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/3/22
+ * Time: 11:18
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "tags")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+            },
+            mappedBy = "tags")
+    @JsonIgnore
+    private Set<Tutorial> tutorials = new HashSet<>();
+
+}
