6f82c3f8fd1187bbd5683d78be374b64cdfeb6fc
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-03 11:26:07 +0700

    #18 - Add deleteTutorial API in TutorialController :bowtie:

--- src/main/java/com/hendisantika/controller/TutorialController.java
+++ src/main/java/com/hendisantika/controller/TutorialController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -77,5 +78,12 @@ public class TutorialController {
         _tutorial.setPublished(tutorial.isPublished());
 
         return new ResponseEntity<>(tutorialRepository.save(_tutorial), HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/tutorials/{id}")
+    public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("id") long id) {
+        tutorialRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
