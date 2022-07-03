c39a940f79dc7e54cfe33088df1fbe6b1732a376
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-03 11:13:23 +0700

    #1 - Init project

---
+++ src/main/java/com/hendisantika/SpringBootManyToManySample1Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootManyToManySample1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootManyToManySample1Application.class, args);
+    }
+
+}
